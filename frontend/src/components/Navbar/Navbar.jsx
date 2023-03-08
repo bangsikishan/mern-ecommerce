3f30c73c9e46f9ca78b87575d6145559ed165a2b
Kishan Bangsi Magar <bang.siki2009@gmail.com>
2023-03-08 19:13:22 +0545

    Add navbar links and styles

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,58 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { SiShopee } from 'react-icons/si';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleClick = () => {
+        setNav(prevNav => !prevNav);
+
+        console.log(nav);
+    }
+
     return (
-            <nav className="flex justify-between py-3 px-12">
-                <div>
+            <nav className="relative flex justify-between h-14 px-5 md:px-12 bg-[#1E1E1E]">
+                <div className="flex justify-center">
                     <Link to='/' className="flex items-center space-x-4">
-                        <SiShopee className="text-4xl" />
-                        <p className="text-2xl font-bold text-[#2E2E2E]">E-Com</p>
+                        <SiShopee className="text-4xl text-white" />
+                        <p className="text-2xl font-bold text-white">E-Com</p>
                     </Link>
                 </div>
-                <ul className="flex items-center">
+                <RxHamburgerMenu onClick={handleClick} className="my-auto text-2xl text-white cursor-pointer md:hidden" />
+                <ul className="hidden md:flex items-center">
                     <li>
-                        <Link className='font-semibold py-2 px-4 mx-2 rounded-lg hover:bg-[#00C9B7] hover:text-[#E8F3F1] transition duration-500 ease-in-out' to='/'>Home</Link>
+                        <Link className='nav-link' to='/'>Home</Link>
                     </li>
                     <li>
-                        <Link className='font-semibold py-2 px-4 mx-2 rounded-lg hover:bg-[#00C9B7] hover:text-[#E8F3F1] transition duration-500 ease-in-out' to='/shop'>Shop</Link>
+                        <Link className='nav-link' to='/shop'>Shop</Link>
                     </li>
                     <li>
-                        <Link className='font-semibold py-2 px-4 mx-2 rounded-lg hover:bg-[#00C9B7] hover:text-[#E8F3F1] transition duration-500 ease-in-out' to='/contact'>Contact Us</Link>
+                        <Link className='nav-link' to='/contact'>Contact Us</Link>
                     </li>
                     <li>
-                        <Link className='font-semibold py-2 px-4 mx-2 rounded-lg hover:bg-[#00C9B7] hover:text-[#E8F3F1] transition duration-500 ease-in-out' to='/login'>Login</Link>
+                        <Link className='nav-link' to='/login'>Login</Link>
                     </li>
                 </ul>
+                {nav ? (
+                    <ul className="absolute top-14 left-0 flex flex-col items-center justify-center space-y-4 h-52 w-full bg-inherit">
+                        <li>
+                            <Link className='nav-link' to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link className='nav-link' to='/shop'>Shop</Link>
+                        </li>
+                        <li>
+                            <Link className='nav-link' to='/contact'>Contact Us</Link>
+                        </li>
+                        <li>
+                            <Link className='nav-link' to='/login'>Login</Link>
+                        </li>
+                    </ul>
+                ) : (
+                    ''
+                )}
             </nav>
     );
 }
